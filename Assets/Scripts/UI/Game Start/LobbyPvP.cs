d46b186e48affd67776014f53c4a7e229ddacc1f
TanHoang01 <tanhm2k1@gmail.com>
2024-10-15 17:39:21 +0700

    fix server port

--- Assets/Scripts/UI/Game Start/LobbyPvP.cs
+++ Assets/Scripts/UI/Game Start/LobbyPvP.cs
@@ -1,8 +1,9 @@
 using TMPro;
-using Unity.Collections;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class LobbyPvP : NetworkBehaviour
 {
@@ -11,11 +12,6 @@ public class LobbyPvP : NetworkBehaviour
     [SerializeField] private TMP_InputField joinRoomIF;
     [SerializeField] private Button createRoomBT;
     [SerializeField] private Button joinRoomBT;
-
-    private static NetworkVariable<FixedString64Bytes> roomID =
-        new NetworkVariable<FixedString64Bytes>(default, 
-            NetworkVariableReadPermission.Everyone, 
-            NetworkVariableWritePermission.Server);
 
     private void Awake()
     {
@@ -34,41 +30,65 @@ public class LobbyPvP : NetworkBehaviour
     {
         string createRoomID = createRoomIF.text;
 
-        if (string.IsNullOrEmpty(createRoomID))
+        createRoomIF.text = ""; // reset input text
+
+        // check input text 
+        if (string.IsNullOrEmpty(createRoomID) || createRoomID.Length != 4 || !IsNumeric(createRoomID))
         {
-            Debug.Log("Invalid ID");
+            Debug.Log("Invalid Room ID");
             return;
         }
 
-        if (!string.IsNullOrEmpty(roomID.Value.ToString()))
+        // check room id in use
+        if (IsPortInUse(int.Parse(createRoomID)))
         {
-            Debug.Log("A room already exists. Please join the existing room.");
-            return;
+            Debug.Log($"Port {createRoomID} is in use.");
         }
+        else
+        {
+            // Update Port
+            NetworkManager.Singleton.GetComponent<UnityTransport>()
+                .ConnectionData.Port = ushort.Parse(createRoomID);
 
-        // Start room as host
-        NetworkManager.Singleton.StartHost();
+            // Start room as host
+            NetworkManager.Singleton.StartHost();
+        }
     }
 
     public void JoinRoom()
     {
         string joinRoomID = joinRoomIF.text;
 
-        if (string.IsNullOrEmpty(joinRoomID))
+        joinRoomIF.text = ""; // reset input text
+
+        // check input text
+        if (string.IsNullOrEmpty(joinRoomID) || joinRoomID.Length != 4 || !IsNumeric(joinRoomID))
         {
             Debug.Log("Invalid Room ID");
             return;
         }
 
-        NetworkManager.Singleton.StartClient();
+        // check room id in use
+        if (IsPortInUse(int.Parse(joinRoomID)))
+        {
+            // Update Port
+            NetworkManager.Singleton.GetComponent<UnityTransport>()
+            .ConnectionData.Port = ushort.Parse(joinRoomID);
+
+            // join room as client
+            NetworkManager.Singleton.StartClient();
+        }
+        else
+        {
+            Debug.Log($"Port {joinRoomID} is not exist.");
+        }          
     }
 
     public override void OnNetworkSpawn()
     {
         if (IsHost)
         {
-            roomID.Value = new FixedString64Bytes(createRoomIF.text);
-            selectChampPvP.RoomID = roomID.Value.ToString();
+            //selectChampPvP.RoomID = roomID.Value.ToString();
             selectChampPvP.gameObject.SetActive(true);
         }
         else if(IsClient)
@@ -80,9 +100,8 @@ public class LobbyPvP : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     private void CheckRoomStatusServerRpc(string joinRoomID)
     {
-        if (roomID.Value.ToString() == joinRoomID && NetworkManager.Singleton.ConnectedClients.Count == 2)
+        if (NetworkManager.Singleton.ConnectedClients.Count == 2)
             NotifyClientJoinRoomClientRpc(true);
-
         else
             NotifyClientJoinRoomClientRpc(false);
     }
@@ -97,9 +116,29 @@ public class LobbyPvP : NetworkBehaviour
         }
         else
         {
-            Debug.Log("join sucess");
-            selectChampPvP.RoomID = roomID.Value.ToString();
+            Debug.Log("join success");
+            //selectChampPvP.RoomID = roomID.Value.ToString();
             selectChampPvP.gameObject.SetActive(true);
         }
+    }
+
+    private bool IsPortInUse(int port)
+    {
+        return System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties()
+            .GetActiveUdpListeners()
+            .Any(p => p.Port == port);
+    }
+
+    // check if string contain all number
+    private bool IsNumeric(string str)
+    {
+        foreach (char c in str)
+        {
+            if (!char.IsDigit(c))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
